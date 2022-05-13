a7e32c6e71ba9b7723db8eea5645b4abf5a3379b
Mala <diallmalad7362o@gmail.com>
2022-05-13 19:38:51 +0200

    commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function myFunction() {
+  var x = document.getElementById("mydiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+// function myFunction() {
+//     var x = document.getElementById("mydiv");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } 
+//     else {
+//         x.style.display = "none";
+      
+//     }
+//   }
